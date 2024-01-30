03e82e4b6e57dd8a761ddea665d6da01aff5ee4a
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-01-30 14:03:00 +0500

    API-through-XML

---
+++ main.js
@@ -0,0 +1,35 @@
+const postsListContainer = document.querySelector(".posts-list-container");
+
+// fetch using XHR
+
+function fetchUsingXHR() {
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+    xhr.responseType = "json";
+    xhr.send();
+
+    xhr.onload = () => {
+
+        if (xhr.status === 200) {
+            displayResults(xhr.response);
+
+        } else {
+
+            console.log("Some Error Ocurred");
+        }
+    };
+}
+
+function displayResults(posts) {
+
+    postsListContainer.innerHTML = posts.map(postItem => `
+    <div class="post-item">
+        <h3>${postItem.title}</h3>
+        <p>${postItem.body}</p>
+    </div>
+    `).join(" ");
+}
+
+fetchUsingXHR();
+
